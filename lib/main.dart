888c61229369411d5d4ba17a931a506984106489
Harish Sekar <harishrsekar@gmail.com>
2023-07-09 01:02:15 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ void main() {
   runApp(const MyApp());
 }
 
-Future <http.Response> fetchData() {
-  return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
-}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -39,44 +37,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late String textData;
 
-  void _incrementCounter() {
-    setState(() {
 
-      _counter++;
-    });
+  Future<void> fetchEarthquakeData() async {
+    final response = await http.get(Uri.parse(
+        'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-01-01&endtime=2023-01-02&limit=3'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        textData = json.encode(data);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          children: [
+            const Text('Data'),
+            Text(textData),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
